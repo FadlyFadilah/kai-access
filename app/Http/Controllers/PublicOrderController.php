17670d7cf8112d45e61ecd168e75602baa149117
koswara0790asep <koswara0790asep@gmail.com>
2022-01-14 17:42:08 +0700

    mengembalikan yang di hapus

--- app/Http/Controllers/PublicOrderController.php
+++ app/Http/Controllers/PublicOrderController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class PublicOrderController extends Controller
 {
@@ -13,7 +14,7 @@ class PublicOrderController extends Controller
         $acceptHeader = $request->header('Accept');
 
         if ($acceptHeader === 'application/json') {
-            $orders = Order::Where(['user_id' => Auth::user()->id])->OrderBy("id", "DESC")->paginate(5)->toArray();
+            $orders = Order::paginate(5)->toArray();
 
             if ($acceptHeader === 'application/json') {
                 $response = [
@@ -49,12 +50,78 @@ class PublicOrderController extends Controller
         }
     }
 
-    public function destroy($id)
+    public function store($id)
     {
         $acceptHeader = request()->header('Accept');
 
         if ($acceptHeader === 'application/json') {
-            $order = Order::find($id);
+            $contentTypeHeader = request()->header('Content-Type');
+
+            if ($contentTypeHeader === 'multipart/form-data; boundary=<calculated when request is sent>') {
+                $ticket = Ticket::find($id)->firstOrFails();
+                $attr = request()->all();
+                $attr['user_id'] = request()->input('user_id');
+                $attr['ticket_id'] = request()->input('ticket_id');
+                $attr['harga'] = $ticket['harga'];
+
+                $order = Order::create($attr);
+
+                return response()->json($order, 200);
+            } else {
+                return response('Unsupported Media Type', 415);
+            }
+        } else {
+            return response('Not Acceptable!', 406);
+        }
+    }
+
+    public function update($slug)
+    {
+        $acceptHeader = request()->header('Accept');
+
+        if ($acceptHeader === 'application/json') {
+            $contentTypeHeader = request()->header('Content-Type');
+
+            if ($contentTypeHeader === 'multipart/form-data; boundary=<calculated when request is sent>') {
+                $input = request()->all();
+
+                $validationRules = [
+                    "nama" => 'required|min:5',
+                    "slug" => 'required|min:5',
+                    "kelas" => 'required|min:5',
+                    'station_id' => 'required|exists:stations,id'
+                ];
+
+                $validator = Validator::make($input, $validationRules);
+
+                if ($validator->fails()) {
+                    return response()->json($validator->errors(), 400);
+                }
+
+                $order = Order::where(['slug' => $slug])->firstOrFail();
+
+                if (!$order) {
+                    abort(404);
+                }
+
+                $order->fill($input);
+                $order->save();
+
+                return response()->json($order, 200);
+            } else {
+                return response('Unsupported Media Type', 415);
+            }
+        } else {
+            return response('Not Acceptable!', 406);
+        }
+    }
+
+    public function destroy($slug)
+    {
+        $acceptHeader = request()->header('Accept');
+
+        if ($acceptHeader === 'application/json') {
+            $order = Order::where(['slug' => $slug])->firstOrFail();
 
             if (!$order) {
                 abort(404);
@@ -62,7 +129,7 @@ class PublicOrderController extends Controller
 
             $order->delete();
 
-            $message = ['message' => 'delete successfully', 'order_id' => $id];
+            $message = ['message' => 'delete successfully', 'train_slug' => $slug];
             return response()->json($message, 200);
         } else {
             return response('Not Acceptable!', 406);
